f9d0550d45acd72d15a24c50ac18b05446eb21f0
sol <solomonabrahams100@gmail.com>
2021-05-08 22:09:58 +0100

    init

---
+++ Bingo/BingoData.swift
@@ -0,0 +1,57 @@
+//
+//  BingoData.swift
+//  Bingo
+//
+//  Created by Solomon Abrahams on 08/05/2021.
+//
+
+import Foundation
+
+class BingoData: ObservableObject {
+    private static var documentsFolder: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory,
+                                               in: .userDomainMask,
+                                               appropriateFor: nil,
+                                               create: false)
+        } catch {
+            fatalError("Can't find documents directory.")
+        }
+    }
+    private static var fileURL: URL {
+        return documentsFolder.appendingPathComponent("cards.data")
+    }
+    @Published var cards: [BingoCard] = []
+    
+    func load() {
+            DispatchQueue.global(qos: .background).async { [weak self] in
+                guard let data = try? Data(contentsOf: Self.fileURL) else {
+                    #if DEBUG
+                    DispatchQueue.main.async {
+                        self?.cards = BingoCard.data
+                    }
+                    #endif
+                    return
+                }
+                guard let bingoCards = try? JSONDecoder().decode([BingoCard].self, from: data) else {
+                    fatalError("Can't decode saved cards data.")
+                }
+                DispatchQueue.main.async {
+                    self?.cards = bingoCards
+                }
+            }
+    }
+    
+    func save() {
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let cards = self?.cards else { fatalError("Self out of scope") }
+            guard let data = try? JSONEncoder().encode(cards) else { fatalError("Error encoding data") }
+            do {
+                let outfile = Self.fileURL
+                try data.write(to: outfile)
+            } catch {
+                fatalError("Can't write to file")
+            }
+        }
+    }
+}
--- Bingo/ContentView.swift
+++ Bingo/ContentView.swift
@@ -7,10 +7,137 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BingoItem: Identifiable, Codable, Hashable {
+    let id: UUID
+    var title: String
+    var completed: Bool
+    
+    init(id: UUID = UUID(), title: String, completed: Bool) {
+        self.id = id
+        self.title = title
+        self.completed = completed
+    }
+}
+
+struct BingoCard: Identifiable, Codable {
+    let id: UUID
+    var title: String
+    var items: [BingoItem]
+
+    init(id: UUID = UUID(), title: String, items: [BingoItem]) {
+        self.id = id
+        self.title = title
+        self.items = items
+    }
+}
+
+extension BingoCard {
+    static var data: [BingoCard] {
+        [
+            BingoCard(title: "Card1", items: [BingoItem(title: "Item1", completed: false), BingoItem(title: "Item2", completed: false), BingoItem(title: "Item3", completed: false)]),
+            BingoCard(title: "Card2", items: [BingoItem(title: "Item0", completed: false)]),
+        ]
+    }
+}
+
+extension BingoCard {
+    struct Data {
+        var title: String = ""
+        var items: [BingoItem] = []
+    }
+
+    var data: Data {
+        return Data(title: title, items: items)
+    }
+
+    mutating func update(from data: Data) {
+        title = data.title
+        items = data.items
+    }
+}
+
+struct BingoEditor: View {
+    @Binding var card: BingoCard
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ForEach(card.items, id: \.self) { item in
+            TextField("test", text: binding(for: item).title)
+        }
+    }
+    
+    private func binding(for item: BingoItem) -> Binding<BingoItem> {
+        guard let itemIndex = card.items.firstIndex(where: { $0.id == item.id }) else {
+            fatalError("Can't find item in array")
+        }
+        return $card.items[itemIndex]
+    }
+}
+
+struct BingoCreator: View {
+    @Binding var cards: [BingoCard]
+    @State private var data = BingoCard.Data()
+    let saveAction: () -> Void
+    @Environment(\.scenePhase) private var scenePhase
+    
+    var body: some View {
+        VStack {
+            NavigationView {
+                List(cards) { card in
+                    NavigationLink(destination: BingoEditor(card: binding(for: card))) {
+                        Text(card.title)
+                    }
+                }
+                .onChange(of: scenePhase) { phase in
+                    if phase == .inactive { saveAction() }
+                }
+            }
+        }
+    }
+    
+    private func binding(for card: BingoCard) -> Binding<BingoCard> {
+        guard let cardIndex = cards.firstIndex(where: { $0.id == card.id }) else {
+            fatalError("Can't find card in array")
+        }
+        return $cards[cardIndex]
+    }
+}
+
+struct BingoGrid: View {
+    let data = (1...9).map { "Item \($0)" }
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(data, id: \.self) {
+                item in Text(item).frame(minHeight:100)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject private var data = BingoData()
+    
+    var body: some View {
+        TabView {
+            BingoCreator(cards: $data.cards) {
+                data.save()
+            }
+            .onAppear {
+                data.load()
+            }
+            .tabItem {
+                Text("Tab 1")
+            }
+            BingoGrid().tabItem {
+                Text("Tab 2")
+            }
+        }
     }
 }
 
