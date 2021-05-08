9581c9fb1701c9439ea73a347fcc84ae7d77c0bc
sol <solomonabrahams100@gmail.com>
2021-05-08 18:10:21 +0100

    Initial Commit

---
+++ Bingo/BingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BingoApp.swift
+//  Bingo
+//
+//  Created by Solomon Abrahams on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bingo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bingo
+//
+//  Created by Solomon Abrahams on 08/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
